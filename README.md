ade9ccd82d320bffae2f83d1a4530ceeb6a966e0
baileywickham <baileywickham@gmail.com>
2020-09-14 13:26:54 -0700

    add comments/rd

---
+++ README.md
@@ -0,0 +1,6 @@
+# Mersenne Twister
+This is a port of the python random library implimentation of the Mersenne Twister to golang. It is commented based on the origonal paper.
+
+## Links
+[Original paper](http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/ARTICLES/mt.pdf)
+[Python Source Code](https://github.com/python/cpython/blob/master/Modules/_randommodule.c#L75)
--- twister.go
+++ twister.go
@@ -14,13 +14,15 @@ const c = 0xefc60000
 var UPPER_MASK uint = 0x80000000 // U in MT
 var LOWER_MASK uint = 0x7fffffff // ll in MT
 var MATRIX_A uint = 0x9908b0df   // a in MT
-var MAG01 [2]uint
-var mt [N]uint
+var MAG01 [2]uint                // Used in the XOR, a convience feature for the &
+var mt [N]uint                   // Array of N size, used to store the random numbers.
 
 func random(i int) uint {
 	var y uint
 	var kk uint
+
 	// Sets matrix MT to random value
+	// See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. - Python source code
 	initMT(19650218)
 
 	// Used in XOR, defined to make & easier.
@@ -37,15 +39,18 @@ func random(i int) uint {
 		mt[kk] = mt[int(kk)+int((M-N))] ^ (y >> 1) ^ MAG01[y&uint(0x1)]
 	}
 
+	// Step 3 cont.
 	y = (mt[N-1] & UPPER_MASK) | (mt[0] & LOWER_MASK)
 	mt[N-1] = mt[M-1] ^ (y >> 1) ^ MAG01[y&0x1]
 
-	// This should cycle through MT, not be assigned at 0
+	// Step 4, calculate x[i]T where T is the matrix
 	y = mt[i]
-	y ^= (y >> UPPER_MASK)
-	y ^= (y << 7) & b
-	y ^= (y << 15) & c
-	y ^= (y >> 18)
+	y ^= (y >> UPPER_MASK) // 2.2
+	y ^= (y << 7) & b      // 2.3
+	y ^= (y << 15) & c     // 2.4
+	y ^= (y >> 18)         // 2.5
+	// I can't figure out where the 7,15,18 come from. They are only defined
+	// as ints in the paper, and they are hardcoded in the python source code.
 	return y
 }
 
@@ -54,6 +59,8 @@ func initMT(seed uint) {
 	var i uint
 	mt[0] = seed
 	for i = 1; i < N; i++ {
+		// x_0 ... x_N-1 as inital seeds in MT
+		// The generator then generates x_N and so on
 		mt[i] = (uint(1812433253)*(mt[i-1]^(mt[i-1]>>30)) + i)
 	}
 }
